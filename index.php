dc321c20eb5bdb202abb9860becfff452dae68eb
a.ashrafi <a.ashrafi@cotint.ir>
2017-10-18 19:41:30 +0330

    file js

--- index.php
+++ index.php
@@ -3,38 +3,105 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title> کوتاه کننده لینک </title>
 		<link href="css/style.css" rel="stylesheet" type="text/css" />
-		<script src="js/jquery.min.js"></script>
-		<script>
-			$(document).ready(function(){
-				$('.btn').live('click',function(e){
-				    e.preventDefault();
-					var url = $('#url').val();
-					$('.result').html('<img src="images/loading.gif" />');
-					$.post('shortener.php',{url: url},function(data){
-						if(data.short != 'invalid')
-							$('.result').html('<a href="'+data.short+'" target="_blank" >'+data.short+'</a>');
-						else
-							$('.result').html('<div class="error">لینک را درست وارد کنید</div>');
-					},'json');
-				});
-			});
-		</script>
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script>document.documentElement.className = 'js';</script>
 	</head>
-	<body>
-	<div class="title">
-		<H1> کوتاه کننده لینک </h1>
-	</div>
-	    <div class="main">
-			<form id="frm1" action="" method="post">
-				<h2>لینک را در کادر زیر وارد کنید : </h2> <br>
-				<input type="text" id="url" name="url" /><br />
-				<button class="btn">ساخت آدرس </button>
-			</form>
-	    </div>
-		<div class="result">
-		</div>
+	<body class="demo-6">
+    <div class="box-img">
+        <img class="center-img" src="images/logo.png">
+    </div>
+    <main class="main main--demo-6">
+        <div class="content content--demo-6">
+            <div class="hamburger hamburger--demo-6 js-hover">
+                <div class="hamburger__line hamburger__line--01">
+                    <div class="hamburger__line-in hamburger__line-in--01 hamburger__line-in--demo-5"></div>
+                </div>
+                <div class="hamburger__line hamburger__line--02">
+                    <div class="hamburger__line-in hamburger__line-in--02 hamburger__line-in--demo-5"></div>
+                </div>
+                <div class="hamburger__line hamburger__line--03">
+                    <div class="hamburger__line-in hamburger__line-in--03 hamburger__line-in--demo-5"></div>
+                </div>
+                <div class="hamburger__line hamburger__line--cross01">
+                    <div class="hamburger__line-in hamburger__line-in--cross01 hamburger__line-in--demo-5"></div>
+                </div>
+                <div class="hamburger__line hamburger__line--cross02">
+                    <div class="hamburger__line-in hamburger__line-in--cross02 hamburger__line-in--demo-5"></div>
+                </div>
+            </div>
+            <div class="global-menu">
+                <div class="global-menu__wrap">
+                    <a class="global-menu__item global-menu__item--demo-6" href="#">درباره ما</a>
+                    <a class="global-menu__item global-menu__item--demo-6" href="#">تماس باما</a>
+                    <a class="global-menu__item global-menu__item--demo-6" href="#">سایت کوتینت</a>
+                    <a class="global-menu__item global-menu__item--demo-6" href="#">سوالات متداول</a>
+                </div>
+            </div>
+            <svg class="shape-overlays" viewBox="0 0 100 100" preserveAspectRatio="none">
+                <defs>
+                    <linearGradient id="gradient1" x1="0%" y1="0%" x2="0%" y2="100%">
+                        <stop offset="0%"   stop-color="#00c99b"/>
+                        <stop offset="100%" stop-color="#ff0ea1"/>
+                    </linearGradient>
+                    <linearGradient id="gradient2" x1="0%" y1="0%" x2="0%" y2="100%">
+                        <stop offset="0%"   stop-color="#ffd392"/>
+                        <stop offset="100%" stop-color="#ff3898"/>
+                    </linearGradient>
+                    <linearGradient id="gradient3" x1="0%" y1="0%" x2="0%" y2="100%">
+                        <stop offset="0%"   stop-color="#110046"/>
+                        <stop offset="100%" stop-color="#32004a"/>
+                    </linearGradient>
+                </defs>
+                <path class="shape-overlays__path"></path>
+                <path class="shape-overlays__path"></path>
+                <path class="shape-overlays__path"></path>
+            </svg>
+        </div>
+        <div class="box">
+            <form class="form-cont" id="frm1" action="" method="post">
+                <h2 class="center">لینک را در کادر زیر وارد کنید  </h2>
+                <input  class="contoroler " type="text" id="url" name="url" placeholder="لطفا لینک خود را وارد کنید" />
+                <button class="btn">ساخت آدرس </button>
+            </form>
+            <div class="result"></div>
+        </div>
+        <div class="tag-p">
+                <p>
+                    موفقیت خود را اندازه گیری کنید همراه با Hootsuite همراه شوید و تجزیه و تحلیل عمیق را با هر لینک شما به اشتراک بگذارید. سعی کنید Hootsuite رایگان »
+                </p>
+        </div>
+    </main>
 		<div class="footer">
-			<div>Created By : <a href="http://cotint.ir" target="_blank" > COTINT.ir </a></div>
+			<p class="cotint">Powered By Cotint</p>
+            <div class="footer-bord">
+                <ul>
+                    <a href="#"><li>سوالات متداول</li></a>
+                    <a href="#"><li>درباره ما</li></a>
+                    <a href="#"><li>تماس باما</li></a>
+                    <a href="#"><li>صفحه اصلی</li></a>
+                </ul>
+            </div>
 		</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/demo.js"></script>
+    <script src="js/easings.js"></script>
+    <script src="js/demo6.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('.btn').live('click',function(e){
+                e.preventDefault();
+                var url = $('#url').val();
+                $('.result').html('<img src="images/loading.gif" />');
+                $.post('shortener.php',{url: url},function(data){
+                    if(data.short != 'invalid')
+                        $('.result').html('<a href="'+data.short+'" target="_blank" >'+data.short+'</a>');
+                    else
+                        $('.result').html('<div class="error">لینک را درست وارد کنید</div>');
+                },'json');
+            });
+        });
+    </script>
 	</body>
 </html>
