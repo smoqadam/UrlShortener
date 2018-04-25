26e7008b8dfa1915902f843748a067e0ba6b37a2
Ehsan <ehsanyusefzadehasl@gmail.com>
2018-04-25 13:39:30 +0430

    API Page

---
+++ API.php
@@ -0,0 +1,18 @@
+<?php
+echo "<h1 style=\"color: red;\">API</h1>";
+echo "COTINT Corporation Releases API to be used Easily by Everone who wants Shorter Link :D";
+
+echo '<h3 style="color: red;">Post Request</h3>';
+echo "<p>Post Request: http://ctnt.ir/api/v1/shortener/</p>";
+
+echo "<h5 style=\"color: red;\">Variables</h5>";
+echo "<p>You can pass JSON as input</p>";
+
+echo "<h5 style=\"color: red;\">Example</h5>";
+echo '<p>{<br>"url":"http://cotintgroup.ir"<br>}</p>';
+
+echo "<h5 style=\"color: red;\">Output</h5>";
+echo "<p>A JSON file will be returned.</p>";
+
+echo "<h5 style=\"color: red;\">JSON Response Example</h5>";
+echo '<p>{<br>"short":"http://ctnt.ir/5fBTHp2"<br>}</p>';
